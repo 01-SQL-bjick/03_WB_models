f7a976fe4515dc811864d2acf844a72644ad58f5
Patrick Bjick <122363343+bjick84@users.noreply.github.com>
2023-02-20 12:51:35 +0100

    update++

--- 01_FirstProject/sql/02_users.sql
+++ 01_FirstProject/sql/02_users.sql
@@ -2,7 +2,7 @@
 CREATE TABLE IF NOT EXISTS `mydb`.`users` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `username` VARCHAR(45) NOT NULL,
-  `create_time` TIMESTAMP NOT NULL,
+  `create_time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `username_UNIQUE` (`username` ASC))
 ENGINE = InnoDB;
